e0a84d8dec088539a8c160c33de3e50eb6715727
Smruti Ranjan Naik <smrutiranjannaik5@gmail.com>
2026-07-17 11:26:38 +0530

    Add files via upload

---
+++ 0724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length ;
+        int[] ls = new int[n];
+        int[] rs = new int[n];
+        ls[0] = nums[0];
+        rs[n-1] = nums[n-1];
+        for(int i=1;i<n;i++){
+            ls[i]=ls[i-1]+nums[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            rs[i]=rs[i+1]+nums[i];
+        }
+
+        for(int i=0;i<n;i++){
+            if(ls[i]==rs[i]){
+                return i ;
+            }
+        }
+
+        return -1 ;
+    }
+}
